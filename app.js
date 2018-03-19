86baf6d3129f4d949119a7ae2cf9ef639e1be2c7
Xiuling Zhang <xiuling.zhang@f1006-26-34158.bmcc.cuny.edu>
2018-03-19 19:36:04 -0400

    updata JS

--- app.js
+++ app.js
@@ -15,17 +15,17 @@ const questions = [
 
 	{
 	    problem: "1. What is 10 divided by 1?",
-	    option:{A:'',    B:'',    C:''},
+	    option:{A:1,    B:5,    C:10},
 	    answer:''
 	},
 	{
 	    problem: "2. what is the car parking number?",
-	    option:{A:'',    B:'',    C:''},
+	    option:{A:68,    B:86,    C:87},
 	    answer:''
 	},
 	{
 		problem: "3.How many different months have exactly 31 days in 2016?",
-	    option:{A:'',    B:'',    C:''},
+	    option:{A:6,    B:7,    C:4},
 	    answer:''
 	},
 	{
@@ -65,6 +65,9 @@ const questions = [
 	}
 ];
 
+
+
+
 //Show the questions and images
 
 const container = document.getElementById("container");
@@ -83,12 +86,23 @@ for (let i = 0; i < images.length; i++) {
 	img.id = "img"+i;
 	img.classList.add("slide");
 	imgContainer.appendChild(img);
+    
+	console.log(questions[i].option);
+    for (let key in questions[i].option) {
+        let option = questions[i].option[key];
+	   const options = document.createElement('button');
+	   options.classList.add("option");
+	   options.textContent = option;
+        options.onclick = function() {
+            console.log(key);
+            console.log(option);
+            console.log(this);
+        };
+        imgContainer.appendChild(options);
+    }
 	
-	const options = document.createElement('p');
-	options.classList.add("option");
-	options.textContent = questions[i].option;
-	
-    imgContainer.appendChild(options);
+    
+
 	container.appendChild(imgContainer);
 	
 }
